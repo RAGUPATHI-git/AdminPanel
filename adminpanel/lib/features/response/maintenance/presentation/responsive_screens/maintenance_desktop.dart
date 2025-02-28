b1890c4ffe15b53f90bafd526d79cd78b9d32c0b
professor ragu <ragu46270@gmail.com>
2025-03-01 00:09:17 +0530

    new routes file updated by using getx

--- adminpanel/lib/features/response/maintenance/presentation/responsive_screens/maintenance_desktop.dart
+++ adminpanel/lib/features/response/maintenance/presentation/responsive_screens/maintenance_desktop.dart
@@ -1,6 +1,7 @@
 import 'package:adminpanel/core/constants/animations.dart';
 import 'package:adminpanel/core/constants/sizes.dart';
 import 'package:adminpanel/core/constants/texts.dart';
+import 'package:adminpanel/core/utils/device_utility.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -10,39 +11,40 @@ class MaintenanceDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              const SizedBox(
-                width: 330,
-              ),
-              SizedBox(
-                  height: 400,
-                  width: 700,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 0),
-                    child: Lottie.asset(DAnimations.maintenance,
-                        height: 390, width: 590),
-                  )),
-              const SizedBox(
-                height: DSizes.md,
-              ),
-            ],
-          ),
-          Text(
-            Dtext.maintenanceTitle,
-            style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-          ),
-          Text(
-            Dtext.maintenanceSubTitle,
-            style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-                color: Colors.grey),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const SizedBox(
+                  width: 330,
+                ),
+                Expanded(
+                  child: Lottie.asset(DAnimations.maintenance,
+                      height: DeviceUtility.mediaQueryheight(context),
+                      width: DeviceUtility.mediaQueryheight(context)),
+                ),
+                const SizedBox(
+                  height: DSizes.md,
+                ),
+              ],
+            ),
+            Text(
+              Dtext.maintenanceTitle,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            Text(
+              Dtext.maintenanceSubTitle,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey),
+            ),
+          ],
+        ),
       ),
     );
   }
