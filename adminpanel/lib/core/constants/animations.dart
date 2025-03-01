7a040b1c538bf0baeaea5e5d29b13f2c77fb0f0a
Madhumita <madhumitasri24@gmail.com>
2025-03-01 06:17:23 +0530

    error404

---
+++ adminpanel/lib/core/constants/animations.dart
@@ -0,0 +1,7 @@
+class DAnimations {
+  //response
+  static const String maintenance = '../assets/animations/maintenance.json';
+  
+  static const String error404 = '../assets/animations/error404.json';
+
+}
