9a30c0556a39c6428d73e87cdaa8b40e8bb6bf47
professor ragu <ragu46270@gmail.com>
2025-03-01 22:45:23 +0530

    "maintanance screen completed"

--- adminpanel/lib/features/response/maintenance/presentation/responsive_screens/maintenance_tablet.dart
+++ adminpanel/lib/features/response/maintenance/presentation/responsive_screens/maintenance_tablet.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:adminpanel/core/constants/animations.dart';
+import 'package:adminpanel/core/constants/texts.dart';
 
 class MaintenanceTablet extends StatelessWidget {
   const MaintenanceTablet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("This is maintenance screen for tablet"),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Lottie.asset(DAnimations.maintenance, height: 250),
+          const SizedBox(height: 15),
+        const  Text(Dtext.maintenanceTitle,
+              style:
+                   TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 8),
+        const  Text(Dtext.maintenanceSubTitle,
+              style:  TextStyle(fontSize: 16, color: Colors.grey)),
+        ],
       ),
     );
   }
