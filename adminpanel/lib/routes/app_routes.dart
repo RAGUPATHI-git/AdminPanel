9241dc6dde0101aca3810389416602001d63d8a9
Madhumita <madhumitasri24@gmail.com>
2025-03-02 11:58:06 +0530

    '.'

--- adminpanel/lib/routes/app_routes.dart
+++ adminpanel/lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
-import 'package:adminpanel/features/response/error_404/presentation/responsive_screens/error_404_desktop.dart';
+
+import 'package:adminpanel/features/response/error_404/presentation/error_404_screen.dart';
 import 'package:adminpanel/features/response/maintenance/presentation/maintenance_screen.dart';
 import 'package:adminpanel/routes/routes.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
@@ -6,8 +7,8 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 class AppRouting {
 
   final List<GetPage> pages = [
-    GetPage(name: Routes.error_404, page: () => const Error404Desktop()),
+    GetPage(name: Routes.error_404, page: () => const Error404Screen()),
     GetPage(name: Routes.maintenance, page: () => const MaintenanceScreen()),
   ];
-  
+
 }
