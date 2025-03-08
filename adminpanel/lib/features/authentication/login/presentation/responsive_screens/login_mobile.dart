0f959436a1dd630d934b0ead26ba0b16fccef6fa
Madhumita <madhumitasri24@gmail.com>
2025-03-08 09:57:18 +0530

    'Login screen changes'

--- adminpanel/lib/features/authentication/login/presentation/responsive_screens/login_mobile.dart
+++ adminpanel/lib/features/authentication/login/presentation/responsive_screens/login_mobile.dart
@@ -10,87 +10,109 @@ class LoginMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: DGradients.sampleGradient
-        ),
-        child: Stack(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: DColors.standardlightgrey,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color:DColors.standardlightgrey,
-                        spreadRadius: 6,
-                        blurRadius: 5,
-                      ),
-                    ],
-                  ),
-                  height: 400,
-                  width: 500,
+             width: 900,
+            height: 900,
+            child: Stack(
+              children: [
+                Center(
                   child: Padding(
                     padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Log in',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: DGradients.sampleGradient,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color:Color(0x66004d4d),
+                            spreadRadius: 5,
+                            blurRadius: 5,
                           ),
-                        ),
-                        SizedBox(height: 20),
-                        DEmailField(),
-                        SizedBox(height: 20),
-                        DPasswordfield(),
-                        SizedBox(height: 30),
-                        SizedBox(height: 20),
-                
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+                        ],
+                      ),
+                      height: 400,
+                      width: 500,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
                           children: [
-                            TextButton(onPressed: (){}, child: Text('Forgot Password ?',style: TextStyle(color: Colors.white),)),
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                 Text(
+                                  'ADMIN PANEL',
+                                  style: TextStyle(
+                                    color:DColors.standardwhite,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 5),
+                            const Divider(thickness: 2,color: Colors.grey,),
+                            const SizedBox(height: 20),
+                            const DEmailField(),
+                            const SizedBox(height: 20),
+                            const DPasswordfield(),
+                            const SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                TextButton(onPressed: (){}, child: const Text('Forgot Password ?',style: TextStyle(color: Colors.white),)),
+                              ],
+                            )
                           ],
-                        )
-                      ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            Center(
-              child: Column(
-                children: [
-                  SizedBox(height: 560),
-                  ElevatedButton(
-                    onPressed: (){
-                    },
-                    style:ElevatedButton.styleFrom(
-    backgroundColor: const Color(0x66808080) ,
-    foregroundColor: Colors.white,
-    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
-    textStyle:const TextStyle(fontSize: 18,fontWeight: FontWeight.bold),   
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(4),
-    ),
-  ),
-                    child: SizedBox(
-                      height: 40,
-                      width: 180,
-                      child: Center(child: Text('Login to your account')),
-                    ),
+
+
+          
+               const Positioned(
+                top:140,
+                right: 35,
+                 child: CircleAvatar(
+                  radius: 50,
+                  backgroundColor: DColors.customlight,
+                 ),
+               ),
+
+
+
+
+                Positioned(
+                  left: 40,
+                  bottom: 140,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 500),
+                      ElevatedButton(
+                        onPressed: (){
+                        },
+                        style:ElevatedButton.styleFrom(
+                          backgroundColor: DColors.customlight,
+                          foregroundColor: DColors.standardwhite,
+                          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 50),
+                          textStyle:const TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        child: Center(child: Text('Login to your account')),
+                        ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+
+
+
+
+          
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        );
   }
 }
