dc9c14644528706f964e02028e96eeac85d030f1
professor ragu <ragu46270@gmail.com>
2025-03-28 20:45:25 +0530

    "study material inital state"

---
+++ adminpanel/lib/features/study materials/presentation/screens/responsive screens/study_material_tablet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StudyMaterialTablet extends StatelessWidget {
+  const StudyMaterialTablet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
